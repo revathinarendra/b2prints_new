f2759046c310e502b13e1a775bdfa025f3e84497
KATARI REVATHI <katari.revathi@gmail.com>
2024-08-09 22:03:54 +0530

    url

--- orders/utils/file_upload.py
+++ orders/utils/file_upload.py
@@ -1,8 +1,9 @@
 # your_app/utils/file_upload.py
 
 import os
+from dotenv import load_dotenv
 from supabase import create_client, Client
-
+load_dotenv()
 # Load environment variables
 url = os.environ.get('SUPABASE_URL')
 key = os.environ.get('SUPABASE_ANON_KEY')
